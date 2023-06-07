9b68b5f4e269b8b34e50aa214eeaf03ccf167cb6
gustavoLimaOliveira <gustavo93.lima@gmail.com>
2023-06-07 20:22:04 -0300

    Entity produto

---
+++ src/main/java/br/com/fiap/pettech/dominio/produto/entitie/Produto.java
@@ -0,0 +1,86 @@
+package br.com.fiap.pettech.dominio.produto.entitie;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Produto {
+        private UUID id;
+        private String nome;
+        private String descricao;
+        private String urlImagem;
+        private double preco;
+
+        public Produto() {}
+
+    public Produto(UUID id, String nome, String descricao, String urlImagem, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.urlImagem = urlImagem;
+        this.preco = preco;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", urlImagem='" + urlImagem + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
---
+++ src/main/java/br/com/fiap/pettech/dominio/produto/repository/IProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.pettech.dominio.produto.repository;
+
+import java.util.Optional;
+
+public interface IProdutoRepository {
+
+    Optional<>
+}
